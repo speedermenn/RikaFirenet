b0fc80ef5f6998fbf5599ea2da3ca0ba6098b216
Speedermenn <oliver.florent@gmail.com>
2025-01-13 14:50:49 +0100

    New files

--- custom_components/rikafirenet/__init__.py
+++ custom_components/rikafirenet/__init__.py
@@ -1,5 +1,7 @@
 import asyncio
 import logging
 
-
+import requests.exceptions
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
